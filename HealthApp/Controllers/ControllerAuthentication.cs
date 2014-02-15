f6682134af40d98459d28f21bee2f4dd96ddfd02
akossebestyen <akos_sebestyen@hotmail.com>
2014-02-15 15:46:00 -0800

    added the authentication file. it was untracked before.

---
+++ HealthApp/Controllers/ControllerAuthentication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+namespace healthApp.Controllers
+{
+    //controllers should extend this class. It contains methods for authentication methods for the users
+    public class ControllerAuthentication : Controller
+    {
+        
+        public bool hasAdminAccess()
+        {
+            return Request.IsAuthenticated && (User.Identity.Name.ToString() == "sysadmin");
+        }
+
+        public bool hasUserAccess()
+        {
+            //note: name in this case is actually the role of the user not the name
+            return Request.IsAuthenticated && (hasAdminAccess() || (User.Identity.Name.ToString() == "user"));
+        }
+    }
+}
