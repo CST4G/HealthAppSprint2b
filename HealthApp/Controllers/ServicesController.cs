6047ace238bdb4dbad66d446cf0451284c152e53
chapmanc <chapman.chris.h@gmail.com>
2014-02-15 16:26:06 -0800

    s

--- HealthApp/Controllers/ServicesController.cs
+++ HealthApp/Controllers/ServicesController.cs
@@ -36,7 +36,7 @@ namespace healthApp.Controllers
 
                 Tasks sc = new Tasks();
                 //populate schedule with new records
-                foreach (var item in tasks)
+                foreach (var item in tasks.ToList())
                 {
                     sc.taskID = item.ID;
                     sc.PatientID = item.PatientID;
@@ -45,9 +45,10 @@ namespace healthApp.Controllers
                     sc.duration = item.duration;
                     sc.tDate = DateTime.Today + item.dtStart.TimeOfDay;
                     db.Tasks.Add(sc);
+                    db.SaveChanges();
                 }
 
-                db.SaveChanges();
+                
                 return View(db.Tasks.ToList());
             }
             else
