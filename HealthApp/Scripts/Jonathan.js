abe5de0dc0bbd2408dc88776608d7ce6db2e42e7
chapmanc <chapman.chris.h@gmail.com>
2014-02-19 20:49:18 -0800

    styling hardcore like a beast

--- HealthApp/Scripts/Jonathan.js
+++ HealthApp/Scripts/Jonathan.js
@@ -2,4 +2,13 @@
     $('[data-toggle=offcanvas]').click(function () {
         $('.row-offcanvas').toggleClass('active');
     });
+    var url = window.location;
+    // Will only work if string in href matches with location
+    $('ul.nav a[href~="' + url + '"]').parent().addClass('active');
+
+    // Will also work for relative and absolute hrefs
+    $('ul.nav a').filter(function () {
+        return this.href == url;
+    }).parent().addClass('active');
 });
+
