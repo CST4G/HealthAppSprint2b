3cad321b22ccac09ad5e39618b4db1e6538c6a4e
Mark Niehe <mniehe@gmail.com>
2014-02-21 00:17:38 -0800

    Added Wills implementation of managing standard pdf forms

---
+++ HealthApp/Models/PDFForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace healthApp.Models {
+    public class PDFForm {
+        //one variable for each feild in the table
+        public int ID { get; set; }
+        //Title of the document for displaying on the webapp eg. Patient Discharge Form 
+        public string Title { get; set; }
+        //fileName of the document for storing eg. patientDischargeForm  note does not contain .pdf
+        public string fileName { get; set; }
+    }
+
+    public class FormDBContext : DbContext {
+        public FormDBContext()
+            : base( "DefaultConnection" ) {
+        }
+        public DbSet<PDFForm> PDFForms { get; set; }
+    }
+}
